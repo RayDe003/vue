9b4e9c8bac1ef0beec7eb64352c3a2b65da5feb0
RayDe00 <kanal.moy@inbox.ru>
2024-02-06 16:27:59 +0700

    Пр 2 и 3

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 let app = new Vue({
     el: '#app',
     data: {
-        product: "Socks"
+        product: "Socks",
+        description: " A pair of warm, fuzzy socks.",
+        // image: "./img/vmSocks-green-onWhite.jpg",
+        image: "./img/vmSocks-blue-onWhite.jpg",
+        altText: "A pair of socks",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks.",
+        inStock: true,
+        inventory: 9,
+        onSale: false
     }
 })
